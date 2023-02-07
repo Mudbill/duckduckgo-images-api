cc29765696b4d7de52aa8dd7fe4c79074b7d81b1
Mudbill <mudbill.b@gmail.com>
2023-02-07 23:44:26 +0100

    Formatting with Prettier

--- src/utils.js
+++ src/utils.js
@@ -1,37 +1,33 @@
-const axios = require('axios');
-const {url} = require('./constants')
+const axios = require("axios");
+const { url } = require("./constants");
 
 function sleep(ms) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms)
-    })
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
 async function getToken(keywords) {
+  let token = null;
+  try {
+    let res = await axios.get(url, {
+      params: {
+        q: keywords,
+      },
+    });
 
-    let token = null;
-    try {
-        let res = await axios.get(url, {
-            params: {
-                q: keywords
-            }
-        })
+    token = res.data.match(/vqd=([\d-]+)\&/)[1];
+  } catch (error) {
+    console.error(error);
+  }
 
-        token = res.data.match(/vqd=([\d-]+)\&/)[1]
-
-    } catch (error) {
-        console.error(error)
-    }
-
-    return new Promise((resolve, reject) => {
-        if (!token)
-            reject('Failed to get token')
-        resolve(token)
-    })
-
+  return new Promise((resolve, reject) => {
+    if (!token) reject("Failed to get token");
+    resolve(token);
+  });
 }
 
-module.exports ={
-    sleep,
-    getToken
-}
+module.exports = {
+  sleep,
+  getToken,
+};
